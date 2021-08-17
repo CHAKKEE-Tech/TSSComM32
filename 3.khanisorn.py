de13b94b343a304ca2744cf3573c91fdb97de02b
ToNpa1M <88695620+ToNpa1M@users.noreply.github.com>
2021-08-17 11:27:08 +0700

    add

---
+++ 3.khanisorn.py
@@ -0,0 +1,39 @@
+#Data Type
+age1 = "20"
+age = 20
+print(type(age1), age1)
+print(type(age2), age2)
+
+name = "Khanisorn Reanthong"
+age = 14
+men = True
+women = False
+money = 1000
+
+print(type(name), name)
+print(type(age), age)
+print(type(men), men)
+print(type(women), women)
+print(type(money), money)
+
+number1 = 45
+number2 = 30
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+
+#loop
+for i in range(10):
+    i = i+1
+    if i>5 and i<=7:
+        print(i)
+
+first_name = "Khanisorn"
+last_name = "Reanthong"
+firend = "Finn"
+
+for i in range(10):
+    print(type(first_name), first_name)
+    print(type(last_name), last_name)
+    print(type(firend), firend)
