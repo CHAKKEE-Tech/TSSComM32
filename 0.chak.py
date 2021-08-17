1b4096aec3c4c2de14cd2c558bfb26c99f5e5992
ChakSGT <38743018+ChakSGT@users.noreply.github.com>
2021-08-17 11:06:04 +0700

    ADD

---
+++ 0.chak.py
@@ -0,0 +1,40 @@
+#Data Type
+age1 = "20"
+age2 = 20
+print(type(age1), age1+age1)
+print(type(age2), age2+age2)
+
+name = "Wiroot Klakhaeng"
+age = 27
+men = True
+Women = False
+money = 1000000.01
+
+print(type(name), name)
+print()
+print()
+print()
+print()
+
+#Opertor
+#ตัวดำเนินการทางคณิตศาสตร์
+number1 = 45
+number2 = 30
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+
+#loop
+for i in range(10):
+    i = i+1
+    if i>5 and i<=7:
+        print(i)
+
+first_name = ""
+last_name = ""
+friend = ""
+for j in range(10):
+    print()
+
+
