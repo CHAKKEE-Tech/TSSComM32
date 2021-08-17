1dd58c9d4fa58c966a3d16ab6c7f070f89a7cc32
Cocoapunzz <88695930+Cocoapunzz@users.noreply.github.com>
2021-08-17 11:15:04 +0700

    ADD

---
+++ 2.kaopun.py
@@ -0,0 +1,35 @@
+#Data Type
+name = "Kanasamon Sesthabutra"
+age = 14
+men = False
+Woman = True
+money = 20000.04
+
+print(type(name),name)
+print(type(age),age)
+print(type(men),men)
+print(type(Woman),Woman)
+print(type(money),money)
+
+#Operator
+number1 = 45
+number2 = 30
+
+print(number1 +number2)
+print(number1 -number2)
+print(number1*number2)
+print(number1/number2)
+
+#loop
+for i in range(10):
+    i = i+1
+    if i>5 and i<=7:
+       print(i)
+    
+first_name = "Kanasamon"
+last_name = "Sesthabutra"
+friend = "Fahsai"
+for j in range(10):
+    print(first_name)
+    print(last_name)
+    print(friend)
---
+++ main.py
@@ -0,0 +1,4 @@
+number1 = 20
+number2 = 30
+result = number1 * number2
+print(result)
