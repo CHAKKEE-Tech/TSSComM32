a92c5e2f3c52d8eae00092b053bc688fa3796466
jirusfinn <88695703+jirusfinn@users.noreply.github.com>
2021-08-17 11:18:07 +0700

    add

---
+++ 4.finn.py
@@ -0,0 +1,28 @@
+#Data Type
+
+name = "Jiurs Patthanathamrongkasem"
+age = 14
+men = True
+Women = False
+money = 2.19
+
+print(type(name), name)
+print(type(age), age)
+print(type(men), men)
+print(type(Women), Women)
+
+#Operator
+#ตัวดำเนินการทางวิทยาศาสตร์
+number1 = 45
+number2 = 30
+
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+
+first_name = "Jirus"
+last_name = "Patthanathamrongkasem"
+friend = "Tompalm"
+for j in range(10):
+    print(first_name,last_name,friend)
