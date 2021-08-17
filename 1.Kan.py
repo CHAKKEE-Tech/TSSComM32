32444b05f542f615c568a7a642186f8d5d448249
drkanfi <kannylyy@gmail.com>
2021-08-17 11:07:14 +0700

    ADD

---
+++ 1.Kan.py
@@ -0,0 +1,37 @@
+#Data Type
+
+name = "Kan Permyanwanthana"
+age = 15
+men = True
+women = False
+money = 111.111
+
+print(type(name), name)
+print(type(age), age)
+print(type(men), men)
+print(type(women), women)
+print(type(money), money)
+
+#Operator
+number1 = 10
+number2 = 5
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+
+#Loop
+for i in range(10):
+    i = i+1
+    if i>5 and i<=7:
+        print(i)
+
+first_name = "Kan"
+last_name = "Permyanwanthana"
+friend = "Dawn"
+for i in range(10):
+    print(type(first_name), first_name)
+    print(type(last_name), last_name)
+    print(type(friend), friend)
+
+
